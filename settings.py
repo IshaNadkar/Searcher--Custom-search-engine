7f5e68019aa9c8e95584d7cfcf7fc6575870b83e
Isha Nadkar <ishanadkar@gmail.com>
2022-08-19 23:13:20 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyDrHoqlx4Pxgb6E3QjawtUy0JHqXE6WPvA"
+SEARCH_ID = "c12026ae21f35425d"
+COUNTRY = "in"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
+RESULT_COUNT = 20
